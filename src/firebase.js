81cf86fa0ae98bbd55f2a3fe20dbf7c4d322d58a
Almeida-91 <aplalmeida91@outlook.com>
2023-03-06 21:28:24 +0000

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTNxVgUYnTiSLrs5t1evLKdut0cmBwoO8",
+  authDomain: "mock-web.firebaseapp.com",
+  projectId: "mock-web",
+  storageBucket: "mock-web.appspot.com",
+  messagingSenderId: "771148055589",
+  appId: "1:771148055589:web:d5d8794a2343f7364c6bcc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
