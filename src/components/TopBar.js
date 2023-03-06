15830e60e4bebcb83c681bd7e3c926407d4c56fd
Almeida-91 <aplalmeida91@outlook.com>
2023-03-06 22:16:10 +0000

    Add Google Sign In

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,40 @@
 import "./topbar.css";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../firebase";
+import { useState } from "react";
 
 const TopBar = (props) => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  auth.useDeviceLanguage();
+
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        setUser(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        setUser(null);
+      });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
   return (
     <div className="topBar">
       <p>Mock WebSite</p>
-      <input type={"text"} defaultValue="Search Mock WebSite"></input>
-      <button>Log In</button>
+      <input type={"text"} placeholder="Search Mock WebSite"></input>
+      {user ? (
+        <button onClick={logout}>Log Out</button>
+      ) : (
+        <button onClick={login}>Log In</button>
+      )}
     </div>
   );
 };
